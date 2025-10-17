f6bd7fc8485f6691d67917407ed0c673f20ae804
girl0330 <girls0330@naver.com>
2025-10-17 14:00:45 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     fetch('http://localhost:8888/wedding')
       .then((response) => {
         if (response.ok === false) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+          throw new Error('청첩장 정보를 불러오지 못했습니다. 연결 test')
         }
         return response.json()
       })
