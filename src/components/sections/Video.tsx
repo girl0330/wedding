009727c2ba9c1eaf082e7a4713ef2ce3c518a9a3
girl0330 <girls0330@naver.com>
2025-10-17 23:52:09 +0900

    feat: 동영상 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,11 +1,17 @@
-import styles from '../sections/Heading.module.scss'
+import styles from '../sections/Video.module.scss'
 import classNames from 'classnames/bind'
 import Section from '../shared/Section'
 
 const cx = classNames.bind(styles)
 
 const Video = () => {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video muted={true} autoPlay={true} loop={true} poster="/assets/poster.jpg">
+        <source src="/assets/main.mp4" type="video/webm" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
